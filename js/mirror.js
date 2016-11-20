d9983a6406051b76d1b390fc9f35b72cf9edae79
Tobz <tob.janik@gmail.com>
2016-11-20 13:49:55 +0000

    Added the whole logic

---
+++ js/mirror.js
@@ -0,0 +1,60 @@
+console.log('I got loaded');
+
+mirrorIt = function() {
+ var videoTag = document.querySelector('video');
+
+ updateToggleControls();
+ videoTag.style.transform =  videoTag.style.transform === 'scaleX(-1)' ? '' : 'scaleX(-1)';
+};
+
+// Create the SVG Image
+getSVG = function() {
+  svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  svg.setAttribute('viewBox', '0 0 24 24');
+  svg.setAttribute('fill', '#fff');
+  pathOne = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  pathTwo = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+  pathOne.setAttribute('d', 'M6.99 11L3 15l3.99 4v-3H14v-2H6.99v-3zM21 9l-3.99-4v3H10v2h7.01v3L21 9z');
+  pathTwo.setAttribute('d', 'M0 0h24v24H0z');
+  pathTwo.setAttribute('fill', 'none');
+
+  svg.appendChild(pathOne,pathTwo);
+  return svg;
+};
+
+updateToggleControls = function() {
+  svg = document.querySelector('.mirrorTube-button svg');
+  console.log(svg.title);
+  if(svg.title === undefined || svg.title === ''){
+    svg.style.fill = '#f12b24';
+    svg.style.transform = 'scaleX(-1)';
+    svg.title = 'Mirrored!';
+  }else{
+    svg.style.fill = '#fff';
+    svg.style.transform = '';
+    svg.title = '';
+  }
+};
+
+addToggleControls = function() {
+  controls = document.querySelector('div.ytp-left-controls');
+  if (!controls) return false;
+  newButton = document.createElement('a');
+  newButton.className = 'ytp-button mirrorTube-button';
+  newButton.title = 'Mirror the video';
+  newButton.appendChild(getSVG());
+  controls.appendChild(newButton);
+  return true;
+};
+
+//Start the whole stuff
+init = function() {
+  addToggleControls();
+  newButton.addEventListener('click', mirrorIt);
+}
+
+//AYYYY
+init();
+
+
+
